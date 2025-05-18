9560f51a452a98081c99f78ec6bca4a5a04dd7eb
Dylan-314 <wyb095q@gmail.com>
2025-05-18 16:25:16 +0800

    Delete py directory

--- py/mod/question_generator/test_question_generator.py
+++
@@ -1,8 +0,0 @@
-from langchain.docstore.document import Document
-from mod.question_generator.generator import generate_questions
-
-def test_generate_questions_format():
-    dummy_chunk = Document(page_content="Python is a programming language.")
-    qa = generate_questions([dummy_chunk], n_per_chunk=1, lang="en", model="gpt-4o-mini")  # 便宜模型即可
-    assert qa and isinstance(qa[0], dict)
-    assert "chunk_id" in qa[0] and "question" in qa[0]
